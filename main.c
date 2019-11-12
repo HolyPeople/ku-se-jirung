f329a8925c2a946e0bfcd5f6bb1291f2a080de2a
HolyPeople <holyjirung@konkuk.ac.kr>
2019-11-12 09:36:34 +0000

    change Key pushed area

--- main.c
+++ main.c
@@ -8,13 +8,11 @@ int main(int argc, char *argv[])
 	int key = 0;
     set_conio_terminal_mode();
 	while (1) {
-    	while (!kbhit()) {
-   			/* When key is not pushed */
+    	if (kbhit()) {
+   			key = getKey();
+			printf("%d\r\n", key);
+			if (key == 'q')
+				break;
 		}
-		/* When key is pushed */
-		key = getKey();
-		printf("%d\r\n", key);
-		if (key == 'q')
-			break;
 	}
 }
